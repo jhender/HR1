2a8fac23a22a85235f75cba14bcd152cb5049cc8
Julien Ho <jianhui@gmail.com>
2015-01-07 00:02:45 +0800

    Init

---
+++ app/src/main/java/com/jhdev/hr1/MainActivity.java
@@ -0,0 +1,129 @@
+package com.jhdev.hr1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.ui.ParseLoginBuilder;
+
+/*
+ * checks for login
+ */
+
+public class MainActivity extends ActionBarActivity {
+
+    private static final int LOGIN_REQUEST = 0;
+
+    private TextView titleTextView;
+    private TextView emailTextView;
+    private TextView nameTextView;
+    private Button loginOrLogoutButton;
+
+    private ParseUser currentUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        titleTextView = (TextView) findViewById(R.id.profile_title);
+        emailTextView = (TextView) findViewById(R.id.profile_email);
+        nameTextView = (TextView) findViewById(R.id.profile_name);
+        loginOrLogoutButton = (Button) findViewById(R.id.login_or_logout_button);
+        titleTextView.setText(R.string.profile_title_logged_in);
+
+        loginOrLogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentUser != null) {
+                    // User clicked to log out.
+                    ParseUser.logOut();
+                    currentUser = null;
+                    showProfileLoggedOut();
+                } else {
+                    // User clicked to log in.
+                    ParseLoginBuilder loginBuilder = new ParseLoginBuilder(MainActivity.this);
+                    startActivityForResult(loginBuilder.build(), LOGIN_REQUEST);
+                }
+            }
+        });
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        currentUser = ParseUser.getCurrentUser();
+        if (currentUser != null) {
+            showProfileLoggedIn();
+        } else {
+            showProfileLoggedOut();
+        }
+    }
+
+    /**
+     * Shows the profile of the given user.
+     */
+    private void showProfileLoggedIn() {
+        titleTextView.setText(R.string.profile_title_logged_in);
+        emailTextView.setText(currentUser.getEmail());
+        String fullName = currentUser.getString("name");
+        if (fullName != null) {
+            nameTextView.setText(fullName);
+        }
+        loginOrLogoutButton.setText(R.string.profile_logout_button_label);
+
+        /*
+         * Display button to "Looking for a job" or "I'm hiring."
+         *
+         */
+    }
+
+    /**
+     * Show a message asking the user to log in, toggle login/logout button text.
+     */
+    private void showProfileLoggedOut() {
+        titleTextView.setText(R.string.profile_title_logged_out);
+        emailTextView.setText("");
+        nameTextView.setText("");
+        loginOrLogoutButton.setText(R.string.profile_login_button_label);
+    }
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+
+}
---
+++ app/src/main/java/com/jhdev/hr1/MainApplication.java
@@ -0,0 +1,22 @@
+package com.jhdev.hr1;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Required - Initialize the Parse SDK
+        Parse.initialize(this, getString(R.string.parse_app_id),
+                getString(R.string.parse_client_key));
+
+        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
+
+        // Optional - If you don't want to allow Twitter login, you can
+        // remove this line (and other related ParseTwitterUtils calls)
+//        ParseTwitterUtils.initialize(getString(R.string.twitter_consumer_key),
+//                getString(R.string.twitter_consumer_secret));
+    }
+}
