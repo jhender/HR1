31c5834bd2a6a8282b154467f22687a545dd9225
Julien Ho <jianhui@gmail.com>
2015-01-23 00:30:50 +0800

    added profile

--- app/src/main/java/com/jhdev/hr1/EmployeeProfileActivity.java
+++ app/src/main/java/com/jhdev/hr1/EmployeeProfileActivity.java
@@ -5,13 +5,28 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.ParseUser;
+
 
 public class EmployeeProfileActivity extends ActionBarActivity {
+
+    ParseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employee_profile);
+
+        //todo Check if User is logged in. If not logged in, redirect to login/signup page.
+
+        //todo Check if User has a profile. If do not, prompt to fill up details?
+
+        currentUser = ParseUser.getCurrentUser();
+
     }
+
+
+
+
 
 }
